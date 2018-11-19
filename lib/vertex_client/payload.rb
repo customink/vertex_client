cd4971d8399fe5953281c6ba82e58a46d51846be
Adam Maas <adam.h.maas@gmail.com>
2018-11-19 11:56:01 -0500

    Fixes bug with constant dupping and nil attributes

--- lib/vertex_client/payload.rb
+++ lib/vertex_client/payload.rb
@@ -1,8 +1,6 @@
 module VertexClient
   class Payload
 
-    INIT_HASH = { :'@transactionType' => 'SALE', line_item: [] }.freeze
-
     attr_reader :output
 
     def initialize(input)
@@ -13,7 +11,7 @@ module VertexClient
     def transform
       default_customer = transform_customer(@input.delete(:customer))
       default_seller   = @input.delete(:seller)
-      @output = INIT_HASH.dup
+      @output = init_hash
       @input[:line_items].each_with_index do |line_item, number|
         @output[:line_item] << transform_line_item(
           line_item,
@@ -27,6 +25,10 @@ module VertexClient
 
     private
 
+    def init_hash
+      { :'@transactionType' => 'SALE', line_item: [] }
+    end
+
     def transform_line_item(line_item, number, options={})
       customer_line = line_item.has_key?(:customer) ? transform_customer(line_item[:customer]) : options[:default_customer]
       seller_line   = line_item.has_key?(:seller) ? line_item[:seller] : options[:default_seller]
@@ -37,19 +39,19 @@ module VertexClient
         product:        line_item[:product_code],
         quantity:       line_item[:quantity],
         extended_price: line_item[:price],
-        discount:       line_item[:discount]
-      }
+        discount:       line_item[:discount]  # Don't send if nil
+      }.delete_if {|_key, value| value.nil? }
     end
 
     def transform_customer(customer)
       {
         destination: {
-          street_address_1: customer[:address_1],
-          street_address_2: customer[:address_2],
+          street_address_1: customer[:address_1], # Don't send if nil
+          street_address_2: customer[:address_2], # Don't send if nil
           city:             customer[:city],
           main_division:    customer[:state],
           postal_code:      customer[:postal_code]
-        }
+        }.delete_if {|_key, value| value.nil? }
       }
     end
   end
--- lib/vertex_client/response.rb
+++ lib/vertex_client/response.rb
@@ -1,12 +1,12 @@
 module VertexClient
   class Response
 
-    attr_reader :body, :total_tax, :total_price
+    attr_reader :body, :total_tax, :price_with_tax
 
     def initialize(response)
       @body = response[:vertex_envelope][:quotation_response]
       @total_tax = @body[:total_tax]
-      @total_price = @body[:price_with_tax]
+      @price_with_tax = @body[:total]
     end
   end
 end
