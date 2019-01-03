25558505c6f54d09488da17dbbc40a0790b55852
Adam Maas <amaas@customink.com>
2019-01-03 09:52:52 -0500

    Update tests for updated fallback rates

--- test/fallback_response_test.rb
+++ test/fallback_response_test.rb
@@ -11,13 +11,13 @@ describe VertexClient::FallbackResponse do
   end
 
   it 'calculates tax on the fallback response' do
-    assert_equal BigDecimal.new('141.5'), @response.total
+    assert_equal BigDecimal.new('142.5'), @response.total
     assert_equal BigDecimal.new('135.5'), @response.subtotal
-    assert_equal BigDecimal.new('6.00'), @response.total_tax
+    assert_equal BigDecimal.new('7.00'), @response.total_tax
   end
 
   it 'calculates tax on the fallback response line items' do
     @response.line_items.each { |li| refute_nil li.total_tax }
-    assert_equal BigDecimal.new('6.00'), @response.line_items.last.total_tax
+    assert_equal BigDecimal.new('7.00'), @response.line_items.last.total_tax
   end
 end
