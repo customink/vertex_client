3850ea855e04fdb0c94803317a9db32764026ac3
RJ Pittman <rypitme@gmail.com>
2019-01-24 18:25:27 -0500

    Test Resource::Quotation

---
+++ test/resources/quotation_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+describe VertexClient::Resource::Quotation do
+  include TestInput
+
+  let(:resource){ VertexClient::Resource::Quotation.new(working_quote_params) }
+
+  it 'has an ENDPOINT' do
+    assert_equal 'CalculateTax70', VertexClient::Resource::Quotation::ENDPOINT
+  end
+
+  it 'has a fallback_response' do
+    assert_kind_of VertexClient::Response::QuotationFallback, resource.fallback_response
+  end
+end
