4050b295f65cfd70d9223139400a29a87f7551fc
RJ Pittman <rypitme@gmail.com>
2019-01-24 19:06:30 -0500

    Test Payload::Invoice

---
+++ test/payloads/invoice_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+describe VertexClient::Payload::Invoice do
+  include TestInput
+  let(:payload) do
+    VertexClient::Payload::Invoice.new(working_quote_params)
+  end
+
+  it 'has a body' do
+    assert_equal expected_payload_output.merge({
+      :'@documentNumber' => 'test123',
+      :'@documentDate' => '2018-11-15'
+    }), payload.body
+  end
+
+  describe 'validate!' do
+    it 'raises if the document_number is not included' do
+      assert_raises VertexClient::ValidationError do
+        working_quote_params.delete(:document_number)
+        VertexClient::Payload::Invoice.new(working_quote_params).validate!
+      end
+    end
+
+    it 'raises if the customer state is not included' do
+      assert_raises VertexClient::ValidationError do
+        working_quote_params[:customer].delete(:state)
+        VertexClient::Payload::Invoice.new(working_quote_params).validate!
+      end
+    end
+
+    it 'raises if the customer postal_code is not included' do
+      assert_raises VertexClient::ValidationError do
+        working_quote_params[:customer].delete(:postal_code)
+        VertexClient::Payload::Invoice.new(working_quote_params).validate!
+      end
+    end
+  end
+end
