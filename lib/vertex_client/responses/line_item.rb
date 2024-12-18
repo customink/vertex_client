0045d14633479ed16084acd8fffb7872fefb00d1
Mariya Whitcomb <mariyawhitcomb@gmail.com>
2024-12-18 11:23:30 -0500

    add flexible_code_field to line item

--- lib/vertex_client/responses/line_item.rb
+++ lib/vertex_client/responses/line_item.rb
@@ -2,13 +2,14 @@ module VertexClient
   module Response
     class LineItem
 
-      attr_reader :total_tax, :product, :quantity, :price
+      attr_reader :total_tax, :product, :quantity, :price, :flexible_code_field
 
       def initialize(params={})
         @product        = params[:product]
         @quantity       = params[:quantity] ? params[:quantity].to_i : 0
         @price          = BigDecimal(params[:price] || 0)
         @total_tax      = BigDecimal(params[:total_tax] || 0)
+        @flexible_code_field = params[:flexible_code_field]
       end
     end
   end
